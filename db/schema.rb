cfbb530a16db9edd6d8b1c79a4cb74c21c3b8a4a
Liz <lizelotte.green@gmail.com>
2015-05-23 22:02:45 -0500

    Uploading Images checkpoint completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510182112) do
+ActiveRecord::Schema.define(version: 20150522013349) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20150510182112) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
