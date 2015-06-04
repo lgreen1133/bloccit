119c2df9e2c2a125ec2c45666789ac9fb0a00ffe
Liz <lizelotte.green@gmail.com>
2015-06-04 12:49:50 -0500

    Fixed rspec vote

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -4,10 +4,12 @@ describe Vote do
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do 
-        vote = Vote.new(value: nil)
-        expect(vote.valid?).to eq(false)
-        vote.value = -1 || 1
-        expect(vote.valid?).to eq(true)
+       vote = Vote.new(value: -1)
+       expect(vote.valid?).to eq(true)
+       vote.value = 1
+       expect(vote.valid?).to eq(true)
+       vote.value = 2
+       expect(vote.valid?).to eq(false)
       end
     end
   end
