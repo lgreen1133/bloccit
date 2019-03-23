b32443dc9d35d1450e247bf9337321d7fad03ce7
Liz <lizelotte.green@gmail.com>
2019-03-23 12:24:42 -0500

    factory_girl -> factory_bot, updating rails, spring

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -1,4 +1,4 @@
-FactoryGirl.define do
+FactoryBot.define do
  factory :post do
    title "Post Title"
    body "Post bodies must be pretty long."
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,4 +1,4 @@
-FactoryGirl.define do
+FactoryBot.define do
  factory :user do
    name "Douglas Adams"
    sequence(:email, 100) { |n| "person#{n}@example.com" }
